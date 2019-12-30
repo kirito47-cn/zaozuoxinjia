c1bf171d14cd25e9de35dc591133a27ba4e82d64
1050735952 <1050735952@qq.com>
2019-12-30 19:40:41 +0800

    '登录注册'

--- zaozuoxinjia/src/main.js
+++ zaozuoxinjia/src/main.js
@@ -3,6 +3,13 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import axios from "axios"
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
+
+axios.defaults.baseURL="http://localhost:8888";
+Vue.prototype.axios=axios;
 
 Vue.config.productionTip = false
 
